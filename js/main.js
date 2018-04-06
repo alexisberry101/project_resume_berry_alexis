9c4f5bed9e304292babdfd15dd8fb83ab008e2d4
Alexis Berry <alexisberry@alexiss-mbp-2.attlocal.net>
2018-04-06 19:07:26 -0400

    add custom jQuery to count clicks on heading

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
     $.backstretch("img/resumebackground.jpg");
+    var $easyzoom = $('.easyzoom').easyZoom();
 });
 
-var $easyzoom = $('.easyzoom').easyZoom();
+//To count clicks on my heading div including my name, email and phone number together
+$(document).ready(function() {
+
+$( '#heading' ).on('click', function() {
+  console.log('welcome to my resume site');
+});
+});
